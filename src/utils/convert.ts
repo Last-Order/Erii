4a99b804edf6f2054405ecbbab1ac70292e66632
Eridanus Sora <sora@sound.moe>
2022-12-13 00:09:08 +0800

    add set convert to proxy

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -5,10 +5,12 @@ export function toCamelCase(str: string) {
 }
 
 export function toKebabCase(str: string) {
-    return str[0].toLowerCase() +
+    return (
+        str[0].toLowerCase() +
         str
             .slice(1, str.length)
-            .replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`);
+            .replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`)
+    );
 }
 
 export function convertObjectKey(before: { [index: string]: any }) {
@@ -18,9 +20,25 @@ export function convertObjectKey(before: { [index: string]: any }) {
 }
 
 export function createCamelProxifiedObject(before: { [index: string]: any }) {
+    const originalObject = before;
     return new Proxy(before, {
         get: (target: typeof before, key: string) => {
-            return target[key] ?? target[toKebabCase(key)];
+            return originalObject[key] ?? originalObject[toKebabCase(key)];
+        },
+        set: (target: typeof before, key: string, value: any) => {
+            // 如果赋值的是camel case的key，而驼峰形式key是从kebab case的key proxy来的 则赋值其kebab case形式的key的值
+            if (
+                !key.includes("-") &&
+                (originalObject[key] === undefined ||
+                    originalObject[key] === null) &&
+                originalObject[toKebabCase(key)] !== undefined &&
+                originalObject[toKebabCase(key)] !== null
+            ) {
+                target[toKebabCase(key)] = value;
+            } else {
+                target[key] = value;
+            }
+            return value;
         },
     });
 }
